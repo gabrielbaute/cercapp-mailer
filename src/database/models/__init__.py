ada1c3b1818478d1fd160d3a7365fbab0648cad5
gabrielbaute <gabrielbaute@gmail.com>
2025-09-03 21:57:59 -0400

    Tabla para contactos

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,1 +1,2 @@
 from src.database.models.users_model import Users
+from src.database.models.contacts_model import Contacts
---
+++ src/database/models/contacts_model.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Dict
+
+from src.database.db_config import db
+
+class Contacts(db.Model):
+    __tablename__ = 'contacts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.username}>: {self.email}'
+
+    def to_dict(self) -> Dict:
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'created_at': self.created_at.isoformat()
+        }

ed82a7074df92440904c8ed18da5abdf7da50c2a
gabrielbaute <gabrielbaute@gmail.com>
2025-09-03 21:37:41 -0400

    Errores

---
+++ src/errors/__init__.py
@@ -0,0 +1,8 @@
+from src.errors.exceptions import (
+    InvalidRoleError, 
+    PermissionDeniedError,
+    NotFoundError, 
+    BadRequestError, 
+    UnauthorizedError, 
+    ForbiddenError, 
+    InternalServerError)
